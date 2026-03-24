7e9940a9e443b6eb4db4539e40fc0a464141be10
olafenko <olopikolo666@gmail.com>
2026-03-24 17:07:45 +0100

    feature/added partial class which extend DbContenxt class and override SaveChanges method so when db record is added/deleted/modified it assigns appropriate values to fields in db

---
+++ PDABProjekt/Models/PABProjektEntitiesExtend.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDABProjekt.Models
+{
+    public partial class PABProjektEntities 
+    {
+
+        public override int SaveChanges()
+        {
+
+            var entries = ChangeTracker.Entries();
+
+
+            foreach (var entity in entries)
+            {
+
+                switch (entity.State)
+                {
+
+                    case EntityState.Added:
+                        {
+                            entity.Property("KiedyDodal").CurrentValue = DateTime.Now;
+                            entity.Property("KtoDodal").CurrentValue = "admin";
+                            entity.Property("CzyAktywny").CurrentValue = true;
+                            break;
+                        }
+
+                    case EntityState.Modified:
+                        {
+                            entity.Property("KiedyModyfikowal").CurrentValue = DateTime.Now;
+                            entity.Property("KtoModyfikowal").CurrentValue = "admin";
+                            break;
+                        }
+
+                    case EntityState.Deleted:
+                        {
+
+                            entity.State = EntityState.Modified;
+
+                            entity.Property("KiedyWykasowal").CurrentValue = DateTime.Now;
+                            entity.Property("KtoWykasowal").CurrentValue = "admin";
+                            entity.Property("CzyAktywny").CurrentValue = false;
+                            break;
+                        }
+
+
+
+                }
+
+            }
+            return base.SaveChanges();
+        }
+
+    }
+}
